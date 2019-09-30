7744387f91e7ebf9798de770d8b6ff2eedd57779
artur <artur@lineup.com.ua>
2019-09-30 23:35:28 +0300

    2 labs ready

---
+++ first_lab/Goncharov_Task1.py
@@ -0,0 +1,4 @@
+print("Introducing to programming: Task1")
+print("Artur Goncharov, I-17b")
+print(44 * "Oksana Andrijivna,\n", "Oksana Andrijivna")
+print((11.2/(11.2-211.3))+(1.2/(15.2+11.3)))
---
+++ third_lab/Goncharov_Task3.py
@@ -0,0 +1,8 @@
+#Introduction to programming Task2
+#Artur Goncharov I-17b-3
+from pip._vendor.distlib.compat import raw_input
+
+print("Introducing to programming: Task1")
+print("Artur Goncharov, I-17b")
+x = int(raw_input("please enter value: "))
+print(len(str(x)))
